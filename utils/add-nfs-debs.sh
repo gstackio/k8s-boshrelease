b50c4aff9cd983c756ddafc0077fb6b151e472e8
Benjamin Gandon <bgandon@users.noreply.github.com>
2024-05-31 10:45:41 +0200

    Bump to Bionic

--- utils/add-nfs-debs.sh
+++ utils/add-nfs-debs.sh
@@ -14,15 +14,16 @@ function main() {
     pushd "${RELEASE_DIR}/tmp/debs" > /dev/null
 
         # bosh -d tinynetes ssh \
-        #     --command="sudo apt-get install --verbose-versions \
-        #         --no-install-recommends --download-only nfs-kernel-server"
+        #     --command="sudo apt-get update -qq ; sudo apt-get install -y \
+        #         --verbose-versions --no-install-recommends --download-only \
+        #         nfs-kernel-server"
 
         # bosh -d tinynetes scp k8s:/var/cache/apt/archives/*.deb .
 
         # for deb in *1%3a*.deb; do mv -v "${deb}" "${deb//1%3a/}"; done
 
         for deb in *.deb; do
-            blob_path="nfs-xenial-debs/${deb}"
+            blob_path="nfs-bionic-debs/${deb}"
             bosh add-blob --dir="${RELEASE_DIR}" "${deb}" "${blob_path}"
         done
 
